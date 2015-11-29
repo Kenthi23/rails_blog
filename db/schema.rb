18ca8667a4d7a1ccefaf6a88021b8a498a4f07dc
Kento Yamakawa <kenton0203@gmail.com>
2015-11-29 07:30:40 +0000

    authenticate_author

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129063852) do
+ActiveRecord::Schema.define(version: 20151129065318) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -38,5 +38,23 @@ ActiveRecord::Schema.define(version: 20151129063852) do
 
   add_index "authers", ["email"], name: "index_authers_on_email", unique: true
   add_index "authers", ["reset_password_token"], name: "index_authers_on_reset_password_token", unique: true
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "authors", ["email"], name: "index_authors_on_email", unique: true
+  add_index "authors", ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
 
 end
